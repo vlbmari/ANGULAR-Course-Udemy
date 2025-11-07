2422c4c046c7d7b664fa7cd204890d96be2af03d
Mariana Lourenço <slb.marianavitoria@gmail.com>
2025-11-07 12:05:08 -0300

    Adicionando users

---
+++ src/app/interfaces/user/address.interface.ts
@@ -0,0 +1,7 @@
+export interface IAnddress {
+  rua: string;
+  numero: number;
+  cidade: string;
+  estado: string;
+  pais: string;
+}
---
+++ src/app/interfaces/user/user.interface.ts
@@ -0,0 +1,18 @@
+import { IAnddress } from "./address.interface";
+import { IStatus } from "./status.interface";
+
+export interface IUser {
+  nome: string;
+  email: string;
+  senha: string;
+  idade: number;
+  endereco: IAnddress;
+  telefone: string;
+  ativo: boolean;
+  funcao: string;
+  dataCadastro: string;
+  status: IStatus;
+}
+
+
+
