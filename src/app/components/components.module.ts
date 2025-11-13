369f0c885047814cb2295fba26e06ef8faefb24e
Mariana Lourenço <slb.marianavitoria@gmail.com>
2025-11-13 12:20:31 -0300

    Ajustando top-div e div right

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material-module";
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     declarations: [
-
-    ],
+      UserDetailsComponent,
+      FilterComponent,
+  ],
     imports: [
-        AngularMaterialModule,
+      FormsModule, 
+      AngularMaterialModule,
     ],
-    exports: []
+    exports: [
+      UserDetailsComponent,
+      FilterComponent
+    ]
 })
 export class ComponentsModule{}
