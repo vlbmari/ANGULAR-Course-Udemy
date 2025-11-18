e3874bb2d8601204e5170c95d7f2be38166058d8
Mariana Lourenço <slb.marianavitoria@gmail.com>
2025-11-18 09:47:39 -0300

    Add filter component

--- src/app/angular-material/angular-material-module.ts
+++ src/app/angular-material/angular-material-module.ts
@@ -1,12 +1,21 @@
 import {NgModule} from "@angular/core";
-
+import { MatDividerModule } from "@angular/material/divider";
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
     imports:[
-
+        MatListModule,
+        MatDividerModule,
+        MatFormFieldModule, 
+        MatInputModule
     ],
     exports: [
-
+        MatListModule,
+        MatDividerModule,
+        MatFormFieldModule, 
+        MatInputModule
     ],
 })
 export class AngularMaterialModule{}
