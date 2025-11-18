5a2f9bde923e5104eb45fcef9d8ced1dc0a69ba2
Mariana Lourenço <slb.marianavitoria@gmail.com>
2025-11-18 13:00:30 -0300

    Ajustes form filters

--- src/app/angular-material/angular-material-module.ts
+++ src/app/angular-material/angular-material-module.ts
@@ -1,21 +1,36 @@
 import {NgModule} from "@angular/core";
-import { MatDividerModule } from "@angular/material/divider";
+import {MatDividerModule} from "@angular/material/divider";
 import {MatListModule} from '@angular/material/list';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from "@angular/material/core";
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     imports:[
         MatListModule,
         MatDividerModule,
         MatFormFieldModule, 
-        MatInputModule
+        MatInputModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule
     ],
+    
+    providers:[
+        provideNativeDateAdapter()
+    ],
+
     exports: [
         MatListModule,
         MatDividerModule,
         MatFormFieldModule, 
-        MatInputModule
+        MatInputModule, 
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule
     ],
 })
 export class AngularMaterialModule{}
